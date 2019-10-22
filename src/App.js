5fade99766b892ce566148b04b4d8f8634908bcc
Andrés <ga.alaix564@gmail.com>
2019-10-22 11:04:37 -0500

    Reto 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Mí primera aplicación</h1>
+      <p>Esto está padrísimo!</p>
+    </div>
+  );
+}
+
+export default App;
